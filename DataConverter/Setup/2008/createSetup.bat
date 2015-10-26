32d5ea7ff830a97e80aa5496e95ca00ec23da236
Weise, Dennis <weise@initions.com>
2015-10-26 08:17:16 +0000

    removed setup files from main project
    git-tfs-id: [http://svc-tfsbi.initions.local:8080/tfs/BICollection]$/initions-BI/Henry Components/DataConverter/Dev/Dev;C1384

--- DataConverter/Setup/2008/createSetup.bat
+++
@@ -1,4 +0,0 @@
-﻿
-"C:\Program Files (x86)\WiX Toolset v3.7\bin\candle.exe"  setup.wxs 
-"C:\Program Files (x86)\WiX Toolset v3.7\bin\light.exe" -out "Setup DataConverter IS2008" -ext WixUIExtension -loc "C:\Program Files (x86)\Windows Installer XML v3\WixUI_de-de.wxl" setup.wixobj 
-echo Fertig
